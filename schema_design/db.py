9ebd8c138d096c5345ac634eebd48f6928ccde59
kseravin <kseravin@freeconferencecall.com>
2024-03-20 10:38:30 +0300

    clean db loader script

--- schema_design/db.py
+++ schema_design/db.py
@@ -9,7 +9,6 @@ from psycopg2.extras import execute_batch
 
 fake = Faker()
 
-# Подготавливаем DSN (Data Source Name) для подключения к БД Postgres
 dsn = {
     'dbname': 'movies_database',
     'user': 'app',
@@ -24,18 +23,13 @@ PAGE_SIZE = 5000
 
 now = datetime.utcnow()
 
-# Установим соединение с БД используя контекстный менеджер with.
-# В конце блока автоматически закроется курсор (cursor.close())
-# и соединение (contextlib.closing вызовет conn.close())
 with contextlib.closing(psycopg2.connect(**dsn)) as conn, conn.cursor() as cur:
-    # Заполнение таблицы Person
     persons_ids = [str(uuid.uuid4()) for _ in range(PERSONS_COUNT)]
     query = 'INSERT INTO person (id, full_name, created, modified) VALUES (%s, %s, %s, %s)'
     data = [(pk, fake.last_name(), now, now) for pk in persons_ids]
     execute_batch(cur, query, data, page_size=PAGE_SIZE)
     conn.commit()
 
-    # Заполнение таблицы PersonFilmWork
     person_film_work_data = []
     roles = ['actor', 'producer', 'director']
 
