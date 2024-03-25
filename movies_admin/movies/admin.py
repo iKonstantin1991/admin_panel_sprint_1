e8df282a6b89cb63d8f22a0a788d354e22707ff2
kseravin <kseravin@freeconferencecall.com>
2024-03-25 10:17:38 +0300

    improve admin panel

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,24 +1,43 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork
 
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
-    raw_id_fields = ('genre',)
+    autocomplete_fields = ('genre',)
 
 
 class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmwork
-    raw_id_fields = ('person',)
+    autocomplete_fields = ('person',)
 
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline, PersonFilmworkInline)
-    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'genre_list', 'person_list')
     list_filter = ('type',)
     search_fields = ('title', 'description', 'id')
+
+    list_prefetch_related = ('genres', 'personas')
+
+    def get_queryset(self, request):
+        queryset = (
+            super()
+            .get_queryset(request)
+            .prefetch_related(*self.list_prefetch_related)
+        )
+        return queryset
+
+    @admin.display(description=_('genres'))
+    def genre_list(self, obj):
+        return ', '.join([genre.name for genre in obj.genres.all()])
+
+    @admin.display(description=_('personas'))
+    def person_list(self, obj):
+        return ', '.join([person.full_name for person in obj.personas.all()])
 
 
 @admin.register(Genre)
